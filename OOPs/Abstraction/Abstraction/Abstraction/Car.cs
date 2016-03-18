391fbb2981ba80170146fae8cb3479bb87341468
Varsha Tiwari <varsha.tiwari@sonata-software.com>
2016-03-18 15:51:41 +0530

    Newly Added code for OOPS concepts in C#.

---
+++ OOPs/Abstraction/Abstraction/Abstraction/Car.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public class Car
+    {
+        //necessary methods and properties exposed by using "public" access modifier &
+        //unnecessary methods and properties (not compulsary) hidden by using "private" 
+        //access modifier.
+        private string _nameofcar = "My Car";
+
+        private string _colorofcar = "Red";
+
+        public string NameofCar
+        {
+            set { _nameofcar = value; }
+            get { return _nameofcar; }
+        }
+
+        public string ColorofCar
+        {
+            set
+            {
+                _colorofcar = value;
+            }
+
+            get
+            {
+                return _colorofcar;
+            }
+
+        }
+
+        public void Steering()
+        {
+            Console.WriteLine("Streering of Car");
+        }
+
+
+        public void RearViewMirror()
+        {
+            Console.WriteLine("RearViewMirror of Car");
+        }
+
+
+
+        public void Brakes()
+        {
+            Console.WriteLine("Brakes of Car");
+        }
+
+        public void Gear()
+        {
+            Console.WriteLine("Gear of Car");
+        }
+
+
+        private void InternalDetailsofCar()
+        {
+            Console.WriteLine("InternalDetailsofCar of Car");
+        }
+
+
+
+        private void CarEngine()
+        {
+            Console.WriteLine("CarEngine of Car");
+        }
+
+        private void DiesalEngine()
+        {
+            Console.WriteLine("DiesalEngine of Car");
+        }
+
+        private void ExhaustSystem()
+        {
+            Console.WriteLine("ExhaustSystem of Car");
+        }
+
+        private void Silencer()
+        {
+            Console.WriteLine("Silencer of Car");
+        }
+    }
+}
---
+++ OOPs/Abstraction/Abstraction/Abstraction/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car objCar = new Car();
+            string name = objCar.NameofCar;
+            objCar.Steering();
+            objCar.RearViewMirror();
+            objCar.Brakes();
+            //Cannot access an example of abstaction/
+            //objCar.CarEngine();
+        }
+    }
+
+
+    /// <summary>
+    /// Abstraction means putting all the variables and methods in a class that are necessary.
+    /// For example: Abstract class and abstract method.
+    /// Abstraction is a common thing.
+    /// </summary>
+    abstract class MobilePhone
+    {
+        public void Calling();
+        public void SendSMS();
+    }
+    public class Nokia1400 : MobilePhone
+    {
+    }
+    public class Nokia2700 : MobilePhone
+    {
+        public void FMRadio();
+        public void MP3();
+        public void Camera();
+    }
+    public class BlackBerry : MobilePhone
+    {
+        public void FMRadio();
+        public void MP3();
+        public void Camera();
+        public void Recording();
+        public void ReadAndSendSMS();
+    }
+}
